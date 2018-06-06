77d224fa64d70088b9a8b98c05e409d0a0856b3f
zhouyongshen <954649679@qq.com>
2018-06-06 15:32:49 +0800

    readme.md

--- interview/src/main/java/com/zys/test/q005/Test.java
+++ interview/src/main/java/com/zys/test/q005/Test.java
@@ -1,5 +1,9 @@
 package com.zys.test.q005;
 
+/*
+Q:switch语句能否作用在byte上，能否作用在long上，能否作用在String上?
+A:在switch（expr1）中，expr1只能是一个整数表达式或者枚举常量（更大字体），整数表达式可以是int基本类型或Integer包装类型，由于，byte,short,char都可以隐含转换为int，所以，这些类型以及这些类型的包装类型也是可以的。显然，long和String类型都不符合switch的语法规定，并且不能被隐式转换成int类型，所以，它们不能作用于swtich语句中。
+ */
 public class Test {
 
     public static void main(String[] args) {
@@ -48,11 +52,13 @@ public class Test {
 
 
     }
-    enum MyEnum{
-        A,B
+
+    enum MyEnum {
+        A, B
     }
-    enum MyEnum2{
-        A,B
+
+    enum MyEnum2 {
+        A, B
     }
 }
 
