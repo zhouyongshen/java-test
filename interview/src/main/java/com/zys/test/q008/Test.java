0f8a2127bf6fbadf78e464ed3e8925ee95cb2fe4
zhouyongshen <954649679@qq.com>
2018-06-07 10:33:44 +0800

    面试题08

---
+++ interview/src/main/java/com/zys/test/q008/Test.java
@@ -0,0 +1,17 @@
+package com.zys.test.q008;
+
+/*
+Q:用最有效率的方法算出2乘以8等於几?
+A:
+    2 << 3，
+    因为将一个数左移n位，就相当于乘以了2的n次方，
+    那么，一个数乘以8只要将其左移3位即可，而位运算cpu直接支持的，效率最高，
+    所以，2乘以8等於几的最效率的方法是2 << 3。
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(2<<3);
+        System.out.println(2<3);
+        System.out.println(16>>3);
+    }
+}
